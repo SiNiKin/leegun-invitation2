90ffc43157e4cc1322a86f7fc90ec2aa58cc7bb1
SiNiKin <tlsdl99@gmail.com>
2025-05-24 01:18:25 +0900

    test

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "2025년 06월 21일 토";
+export const WEDDING_DATE = "2025년 06월 21일 (토)";
 export const WEDDING_TIME = "13:00 ~ 15:00";
 export const WEDDING_LOCATION = "대전광역시 대덕문화원";
 
