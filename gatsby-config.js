49392141fa31711ab878880c167f0242ed4b4627
SiNiKin <tlsdl99@gmail.com>
2025-05-23 20:57:25 +0900

    hi

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  pathPrefix: "/leegun-invitation",
   siteMetadata: {
     title: `weddingInvitation`,
-    siteUrl: `https://SiNiKin.github.io/leegun-invitation`
+    siteUrl: `https://sinikin.github.io/leegun-invitation`,
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  pathPrefix: "/leegun-invitation",
+  plugins: ["gatsby-plugin-styled-components"],
 };
